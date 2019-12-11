ccb0c6267f78f3c0de4876a26905384789945bd4
wolfpan <haoyupan@aliyun.com>
2019-12-11 15:46:11 +0800

    add dist

---
+++ dist/precache-manifest.1350188d584080c2bd2ad878b084b031.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81c39dc2eb55133547f1",
+    "url": "/assets/css/app.a5ec26b2.css"
+  },
+  {
+    "revision": "3b3eed7ec34a11b67560",
+    "url": "/assets/css/chunk-352f1c45.0e433876.css"
+  },
+  {
+    "revision": "3d2c3038ecf7efe6a2ed",
+    "url": "/assets/css/chunk-45f48b58.5163b175.css"
+  },
+  {
+    "revision": "b903297ed062d87892af92bd456bd64c",
+    "url": "/assets/img/icomoon.b903297e.svg"
+  },
+  {
+    "revision": "81c39dc2eb55133547f1",
+    "url": "/assets/js/app.31bdb03a.js"
+  },
+  {
+    "revision": "3b3eed7ec34a11b67560",
+    "url": "/assets/js/chunk-352f1c45.c4c43193.js"
+  },
+  {
+    "revision": "3d2c3038ecf7efe6a2ed",
+    "url": "/assets/js/chunk-45f48b58.e92ad4f6.js"
+  },
+  {
+    "revision": "560131b0371d25de34b8",
+    "url": "/assets/js/chunk-vendors.f4b7bbda.js"
+  },
+  {
+    "revision": "004a6a0a00c0b0e1f2aef7977ea6efb4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c93c49979ed8bbff7c61844bda6064c",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
